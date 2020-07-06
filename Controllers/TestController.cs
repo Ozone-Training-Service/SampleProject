744b27e1a842628c7ab6fc79a2e5d96fbb559bb7
vishnukumarps <vishnukumar29518@gmail.com>
2020-07-06 10:07:22 +0530

    Partial View added

---
+++ Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleProject.Controllers
+{
+    public class TestController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        public IActionResult Index2()
+        {
+
+            return PartialView("PartialView1");
+        }
+    }
+}
